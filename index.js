a5f97e8e2e2b4946d49386c261d05a5329b47e83
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-04-08 22:29:36 -0400

    Express learning

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.all('*', (request, response) => response.status(404).sendFile(path.join(__dirname, 'public', '404.html')))
+
+app.listen(1337, () => {
+  console.log('Listening on port 1337...') // eslint-disable-line no-console
+})
